8ed0f452a06710ca44dca606ff465dfc58b860c2
Mateus Filipe <mateusfilipe2k2@gmail.com>
2023-06-23 23:18:32 -0300

    init

---
+++ src/consumer/consumer.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const amqp = require('amqplib');
+
+const {
+  RABBITMQ_HOST,
+  RABBITMQ_PORT,
+  RABBITMQ_USERNAME,
+  RABBITMQ_PASSWORD,
+  QUEUE_NAME,
+} = process.env;
+
+async function connect() {
+  try {
+    const connection = await amqp.connect({
+      hostname: RABBITMQ_HOST,
+      port: RABBITMQ_PORT,
+      username: RABBITMQ_USERNAME,
+      password: RABBITMQ_PASSWORD,
+    });
+
+    const channel = await connection.createChannel();
+    await channel.assertQueue(QUEUE_NAME);
+
+    channel.consume(QUEUE_NAME, (message) => {
+      console.log('Message received:', message.content.toString());
+      channel.ack(message);
+    });
+  } catch (error) {
+    connect()
+  }
+}
+
+connect();
---
+++ src/producer/producer.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+const amqp = require('amqplib');
+
+
+let {
+    RABBITMQ_HOST,
+    RABBITMQ_PORT,
+    RABBITMQ_USERNAME,
+    RABBITMQ_PASSWORD,
+    QUEUE_NAME,
+    MESSAGE_SIZE,
+    MESSAGES_PER_SECOND,
+} = process.env;
+
+const size = process.argv[2]; 
+const rate = process.argv[3]; 
+if(size && rate){
+    MESSAGE_SIZE = size;
+    MESSAGES_PER_SECOND = rate;
+}
+
+const interval = Math.floor(1000 / Number(MESSAGES_PER_SECOND));
+
+async function connect() {
+    try {
+        const connection = await amqp.connect({
+            hostname: RABBITMQ_HOST,
+            port: RABBITMQ_PORT,
+            username: RABBITMQ_USERNAME,
+            password: RABBITMQ_PASSWORD,
+        });
+
+        const channel = await connection.createChannel();
+        await channel.assertQueue(QUEUE_NAME);
+
+        setInterval(() => {
+            const message = [...Array(Number(MESSAGE_SIZE))].map(() => Math.random().toString(36).charAt(2)).join('');
+            channel.sendToQueue(QUEUE_NAME, Buffer.from(message));
+            console.log('Message sent:', message);
+        }, interval);
+    } catch (error) {
+        // console.error('Producer error:', error);
+        connect()
+    }
+}
+
+connect();
